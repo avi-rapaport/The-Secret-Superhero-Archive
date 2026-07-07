0bff73f2d9d60f37606a00d10578d2b9119034a3
avi rapaport <kenblock.ar@gmail.com>
2026-07-07 11:17:10 +0300

    added getherobyid function and endpoint with path params

--- routes.js
+++ routes.js
@@ -1,12 +1,33 @@
-import { getHeroes } from "./service.js";
+import { getHeroes, getHeroById } from "./service.js";
 
 export async function handleRoutes(req, res, parsedUrl) {
   const method = req.method;
   const pathParts = parsedUrl.pathname.split("/");
 
-  if (method === "GET") {
-    const heroes = await getHeroes();
-    res.writeHead(200);
-    return res.end(JSON.stringify(heroes));
+  if (pathParts.length === 2 && pathParts[1] === "heroes") {
+    if (method === "GET") {
+      const heroes = await getHeroes();
+      res.writeHead(200);
+      return res.end(JSON.stringify(heroes));
+    }
+  } else if (pathParts.length === 3 && pathParts[1] === "heroes") {
+    const heroId = parseInt(pathParts[2]);
+
+    if (isNaN(heroId)) {
+      res.writeHead(400);
+      return res.end(JSON.stringify({ message: "invalid id!" }));
+    }
+    if (method === "GET") {
+      const hero = await getHeroById(heroId);
+
+      if (!hero) {
+        res.writeHead(404);
+        return res.end(
+          JSON.stringify({ message: `hero with id ${heroId} not found!` }),
+        );
+      }
+      res.writeHead(200);
+      return res.end(JSON.stringify(hero));
+    }
   }
 }
--- service.js
+++ service.js
@@ -4,3 +4,8 @@ export async function getHeroes() {
   const data = await readData();
   return data;
 }
+
+export async function getHeroById(heroId) {
+  const data = await readData();
+  return data.find((hero) => hero.id === heroId);
+}
